0a47d1c3b492eecf52e2f15d682ae4e5d73627a8
Erik Castro <erik@developcode.us>
2022-01-27 12:29:19 -0500

    first

---
+++ solidity_contracts/mycontract.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract MyContract {
+
+    string value;
+
+    constructor() {
+        value = "Eriks First Contract";
+    }
+
+
+    function get() public view returns(string memory) {
+        return value;
+    }
+
+    function set(string memory _value) public {
+        value = _value;
+    }
+
+}
+
